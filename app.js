e4b3fe42e0b72dd591ed0e763b04aeadc16f43e9
pranetkhetan <109355228+pranetkhetan@users.noreply.github.com>
2023-06-18 13:03:32 +0530

    updated prompt and reduced temperature

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/answer', function(req,res){
     questionset = questionset + ", question2: '" + package.q2 + "'"
     questionset = questionset + ", answer2: '" + package.a2 + "'"
     questionset = questionset + ", answerkey2: '" + package.akey2 + "' }"
-    prompt = 'Read this prompt in detail and be prepared to answer all questions: You will be provided with three values: a question, an answer, and an answer key in the format { question1: <question goes here>, answer1: <answer goes here>, answerkey1: <answer key goes here>, question2:<question goes here>, answer2: <answer goes here>, answerkey2: <answer key goes here> }. You have to compare the answer to the answer key and give it a score out of three (You are being used to compare and grade student answers). A few pointers to keep in mind: 1) The answers are long answers and don\'t necessarily have to be word-to-word similar. They can be paraphrased or written in a different way but MUST CONTAIN ALL IMPORTANT KEYWORDS in the answer key, 2) Also compare the amount of DETAIL in the answer. You CANNOT give it a perfect score if it is missing any important detail or keyword. TO REACH A PERFECT SCORE, THE ANSWER MUST CONTAIN ALL KEYWORDS AND PROCESSES. A ONE-LINER WILL ONLY GET 1 MARK, 3) You are allowed to give DECIMAL scores, 4) Be very STRICT with your correction. If the answer is NOT SATISFACTORY, give a 1. The ONLY format you can use to reply is - {"score1":"<your score for answer 1>","feedback1":"<your feedback for answer 1>","score2":"<your score for answer 2>","feedback2":"<your feedback for answer 2>"} The answers you have to grade are: '
+    prompt = 'Read this prompt in detail and be prepared to answer all questions: You will be provided with three values: a question, an answer, and an answer key in the format { question1: <question goes here>, answer1: <answer goes here>, answerkey1: <answer key goes here>, question2:<question goes here>, answer2: <answer goes here>, answerkey2: <answer key goes here> }. You have to compare the answer to the answer key and give it a score out of three along with detailed feedback. A few pointers to keep in mind: 1) The answers are long answers and don\'t necessarily have to be word-to-word similar. They can be paraphrased or written in a different way but MUST CONTAIN ALL IMPORTANT KEYWORDS in the answer key, 2) Also compare the amount of DETAIL in the answer. You CANNOT give it a perfect score if it is missing any important detail or keyword. TO REACH A PERFECT SCORE, THE ANSWER MUST CONTAIN ALL KEYWORDS AND PROCESSES. A ONE-LINER WILL ONLY GET 1 MARK, 3) You are allowed to give DECIMAL scores, 4) Be very STRICT with your correction. If the answer is NOT SATISFACTORY, give a 1. The ONLY format you can use to reply is - {"score1":"<your score for answer 1>","feedback1":"<your feedback for answer 1>","score2":"<your score for answer 2>","feedback2":"<your feedback for answer 2>"} The answers you have to grade are: '
     prompt = prompt + questionset
     async function runCompletion () {
         const completion = await openai.createCompletion({
@@ -69,7 +69,7 @@ app.post('/answer', function(req,res){
           "prompt": prompt,
           "max_tokens": 1000,
           "n": 1,
-          "temperature": 0.3
+          "temperature": 0.4
         });
         let answer = JSON.parse(completion.data.choices[0].text)
         res.render('result',{ package:package,scores:answer })
