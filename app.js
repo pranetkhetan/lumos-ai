613f7b959d56a072b6f639fe84fbdac447039eaa
pranetkhetan <109355228+pranetkhetan@users.noreply.github.com>
2023-06-20 14:32:05 +0530

    MAJOR: Add Nanonets OCR Custom AI Model

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const fs = require('fs')
+const request = require('request')
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 const configuration = new Configuration({
@@ -12,6 +14,17 @@ const PORT = process.env.PORT || 3030;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+var multer = require('multer');
+var path = require('path')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)) 
+  }
+})
+var upload = multer({ storage: storage });
 
 let worksheets = []
 let wsid = 0
@@ -58,6 +71,54 @@ app.post('/access', function(req,res){
     } else {
         let ws = worksheets[id-1]
         res.render('answer',{ worksheet:ws, id:id })
+    }
+});
+app.post('/ocr',upload.single('file'), function(req,res){
+    ocranswers = ""
+    try {
+        const form_data = {
+            file: fs.createReadStream(req.file.path),
+        }
+        const options = {
+            url : "https://app.nanonets.com/api/v2/OCR/Model/fecc8c52-0752-4f35-8272-2ce494dd4a74/LabelFile/?async=false",
+            formData: form_data,
+            headers: {
+                'Authorization' : 'Basic ' + Buffer.from('c6b8ead3-0ea9-11ee-a77a-46cff35bfcf2' + ':').toString('base64')
+            }
+        }
+        try{    
+            request.post(options, function(err, httpResponse, body) {
+                let output = JSON.parse(body)
+                let prediction = output.result[0].prediction[0].ocr_text
+                let firstline = 'You will get an input in the form of a paragraph of text, that is the output of an OCR algorithm. Can you use your understanding of language and text to correct it? After correcting it, you will find that the text is divided into two parts - Answer 1 and Answer 2. You need to separate these. The ONLY format you can reply is - {"answer1":"<the text for answer 1>", "answer2":"<the text for answer 2>"} The text is - '
+                let prompt = firstline + prediction
+                async function runCompletion () {
+                    const completion = await openai.createCompletion({
+                    "model": "text-davinci-003",
+                    "prompt": prompt,
+                    "max_tokens": 1000,
+                    "n": 1,
+                    "temperature": 0.4
+                    });
+                    try {    
+                        let answer = JSON.parse(completion.data.choices[0].text)
+                        ocranswers = answer
+                        let id = req.body.id
+                        let worksheet = worksheets[id - 1]
+                        res.render('ocredit',{ worksheet:worksheet, id:id, ocr:ocranswers })
+                    } catch(err) {
+                        console.log(err)
+                        res.render('message',{ messageTitle:"Error uploading your answers", messageBody:"This usually happens very rarely. Please try again." })
+                    }
+                }
+                runCompletion();
+            }); 
+        } catch(err){
+            console.log(err)
+            res.render('message',{ messageTitle:"Error uploading your answers", messageBody:"This usually happens very rarely. Please try again." })
+        }
+    } catch {
+        res.render('message',{messageTitle: "No input given", messageBody: "You have not uploaded any file. Please try again."})
     }
 });
 app.post('/answer', function(req,res){
